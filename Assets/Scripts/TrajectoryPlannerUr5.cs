7562f2b2ea48137880e4cb2f7cd5bb1eca4f990e
TobiasUng <44364683+TobiasUng@users.noreply.github.com>
2022-01-14 13:29:15 +0100

    crete math panels and fix left hand

--- Assets/Scripts/TrajectoryPlannerUr5.cs
+++ Assets/Scripts/TrajectoryPlannerUr5.cs
@@ -11,7 +11,7 @@ public class TrajectoryPlannerUr5 : MonoBehaviour
 {
     // Hardcoded variables
     const int k_NumRobotJoints = 6;
-    const float k_JointAssignmentWait = 0.005f;
+    const float k_JointAssignmentWait = 0.01f;
     const float k_PoseAssignmentWait = 0.5f;
 
     // Variables required for ROS communication
