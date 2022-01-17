7ff4542aad44851a0d4849ec2e772ea899088fbf
TobiasUng <44364683+TobiasUng@users.noreply.github.com>
2022-01-17 11:01:32 +0100

    EMS script working

---
+++ Assets/EMSTestScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uduino;
+
+public class EMSTestScript : MonoBehaviour
+{
+    [Range(0, 255)]
+    public int intensity;
+    private void Start()
+    {
+        UduinoManager.Instance.pinMode(6, PinMode.Output);
+        StartCoroutine(BlinkLoop());
+        // StartCoroutine(EMSLoop());
+    }
+
+    IEnumerator BlinkLoop()
+    {
+        while (true)
+        {
+            UduinoManager.Instance.digitalWrite(6, State.HIGH);
+            yield return new WaitForSeconds(5f);
+            UduinoManager.Instance.digitalWrite(6, State.LOW);
+            yield return new WaitForSeconds(5f);
+
+        }
+    }
+}
